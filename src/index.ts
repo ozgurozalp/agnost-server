f7210c04f984f4216aa1140c292b03202f102ad7
Ümit Çakmak <umit.cakmak@altogic.com>
2023-09-20 17:05:50 +0300

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -24,10 +24,10 @@ import { Expression } from "./expression/Expression";
  * @returns {AgnostServerSideClient} The newly created server-side client instance
  */
 const createServerSideClient = (
-	metaManager: any,
-	adapterManager: any
+  metaManager: any,
+  adapterManager: any,
 ): AgnostServerSideClient => {
-	return new AgnostServerSideClient(metaManager, adapterManager);
+  return new AgnostServerSideClient(metaManager, adapterManager);
 };
 
 const META = (global as any).META;
@@ -39,23 +39,23 @@ const ADAPTERS = (global as any).ADAPTERS;
 const agnost = createServerSideClient(META, ADAPTERS);
 
 export {
-	agnost,
-	createServerSideClient,
-	APIBase,
-	AgnostServerSideClient,
-	Storage,
-	Bucket,
-	File,
-	Queue,
-	Task,
-	Database,
-	DatabaseBase,
-	Model,
-	ModelBase,
-	Field,
-	DBAction,
-	Expression,
-	Func,
+  agnost,
+  createServerSideClient,
+  APIBase,
+  AgnostServerSideClient,
+  Storage,
+  Bucket,
+  File,
+  Queue,
+  Task,
+  Database,
+  DatabaseBase,
+  Model,
+  ModelBase,
+  Field,
+  DBAction,
+  Expression,
+  Func,
 };
 
 export * from "./utils/types";
--- src/managers/Func.ts
+++ src/managers/Func.ts
@@ -9,63 +9,63 @@ import { FunctionName } from "../utils/specifics";
  * @class Func
  */
 export class Func<F extends FunctionName> extends APIBase {
-	/**
-	 * The name of the function
-	 * @protected
-	 * @type {string}
-	 */
-	protected name: F;
+  /**
+   * The name of the function
+   * @protected
+   * @type {string}
+   */
+  protected name: F;
 
-	/**
-	 * The metadata of the function object
-	 * @protected
-	 * @type {string}
-	 */
-	protected meta: any;
+  /**
+   * The metadata of the function object
+   * @protected
+   * @type {string}
+   */
+  protected meta: any;
 
-	/**
-	 * The resource adapter of the function
-	 * @protected
-	 * @type {any}
-	 */
-	protected adapter: any;
+  /**
+   * The resource adapter of the function
+   * @protected
+   * @type {any}
+   */
+  protected adapter: any;
 
-	/**
-	 * Creates an instance of function object to run custom helper functions.
-	 * @param {any} metaManager Provides access to the application the version configuration
-	 * @param {any} adapterManager Provides access to actual resource adapters and drivers
-	 * @param {string} name The name of the function
-	 * @throws Throws an exception if metada or adapter of function object cannot be found
-	 */
-	constructor(metaManager: any, adapterManager: any, name: F) {
-		super(metaManager, adapterManager);
-		this.name = name;
-		// Get the metadata of the function
-		this.meta = this.getMetadata("function", name);
-		if (!this.meta) {
-			throw new ClientError(
-				"function_not_found",
-				`Cannot find the function identified by name '${name}'`
-			);
-		}
+  /**
+   * Creates an instance of function object to run custom helper functions.
+   * @param {any} metaManager Provides access to the application the version configuration
+   * @param {any} adapterManager Provides access to actual resource adapters and drivers
+   * @param {string} name The name of the function
+   * @throws Throws an exception if metada or adapter of function object cannot be found
+   */
+  constructor(metaManager: any, adapterManager: any, name: F) {
+    super(metaManager, adapterManager);
+    this.name = name;
+    // Get the metadata of the function
+    this.meta = this.getMetadata("function", name);
+    if (!this.meta) {
+      throw new ClientError(
+        "function_not_found",
+        `Cannot find the function identified by name '${name}'`,
+      );
+    }
 
-		// Get the adapter of the function
-		this.adapter = this.getAdapter("function", this.name);
-		if (!this.adapter) {
-			throw new ClientError(
-				"adapter_not_found",
-				`Cannot find the adapter of the function named '${name}'`
-			);
-		}
-	}
+    // Get the adapter of the function
+    this.adapter = this.getAdapter("function", this.name);
+    if (!this.adapter) {
+      throw new ClientError(
+        "adapter_not_found",
+        `Cannot find the adapter of the function named '${name}'`,
+      );
+    }
+  }
 
-	/**
-	 * Runs the custom helper function. You can call this method with any number of parameters, yet, the parameter structure ideally needs to match with your helper function parameter definitions.
-	 *
-	 * @returns If successful, returns the output of helper function.
-	 * @throws Throws an exception if the helper function cannot be executed.
-	 */
-	async run(...args: any[]): Promise<any> {
-		return await this.adapter.run(this.name, ...args);
-	}
+  /**
+   * Runs the custom helper function. You can call this method with any number of parameters, yet, the parameter structure ideally needs to match with your helper function parameter definitions.
+   *
+   * @returns If successful, returns the output of helper function.
+   * @throws Throws an exception if the helper function cannot be executed.
+   */
+  async run(...args: any[]): Promise<any> {
+    return await this.adapter.run(this.name, ...args);
+  }
 }
